800f1d7517a30458b78c554859d685cdc8767fb0
Reisone <reisonantia@gmail.com>
2023-10-01 23:57:49 -0600

    Create Cuadrado.java
    Initial commit.

---
+++ Computer Systems Engineering (Undergrad)/UNI CSE - Programacion Orientada a Objetos/Actividad 5 - Programas de Herencia y Polimorfismo/Jerarquia de Clases A/Cuadrado.java
@@ -0,0 +1,22 @@
+package JerarquiaClases;
+
+public class Cuadrado extends Figura {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double area() {
+        return lado * lado;
+    }
+}
